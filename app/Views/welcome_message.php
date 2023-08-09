11f78620468fc0b7bc118274d0b079986cc21046
naufal <czopal@gmail.com>
2023-08-09 14:58:54 +0700

    first test branch

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -6,7 +6,7 @@
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
-
+<!-- test git -->
     <!-- STYLES -->
 
     <style {csp-style-nonce}>

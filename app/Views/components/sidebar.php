c37dcbdfdad17b086ebd8076fc68505d0ff53de9
rajamka15 <rmeidyakama@gmail.com>
2023-08-26 05:20:28 +0700

    Update Link Profile

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -15,7 +15,7 @@
           <img src="<?php echo base_url() ?>public/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Raja MKA</a>
+          <a href="<?php echo base_url() ?>user/profile" class="d-block">Raja MKA</a>
         </div>
       </div>
 
